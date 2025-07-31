bfab15c6ea2822bc148ba380c4bd960507b9f747
YMSenanayake <yasirumadhusankasenanayake@gmail.com>
2025-07-31 22:26:05 +0530

    update

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -22,30 +22,30 @@ const PlaceOrder = () => {
   const onChangeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setData(data=>({...data,[name]:value}))
+    setData(data => ({ ...data, [name]: value }))
   }
 
-  const placeOrder = async (event)=>{
+  const placeOrder = async (event) => {
     event.preventDefault();
     let orderItems = [];
-    food_list.map((item)=>{
-      if (cartItems[item._id]>0) {
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
         let itemInfo = item;
         itemInfo["quantity"] = cartItems[item._id];
         orderItems.push(itemInfo)
       }
     })
     let orderData = {
-      address:data,
-      items:orderItems,
-      amount:getTotalCartAmount()+200,
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 200,
     }
-    let response = await axios.post(url+"/api/order/place",orderData,{headers:{token}});
+    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
     if (response.data.success) {
-      const{session_url} = response.data;
+      const { session_url } = response.data;
       window.location.replace(session_url);
     }
-    else{
+    else {
       alert("Error");
     }
   }
